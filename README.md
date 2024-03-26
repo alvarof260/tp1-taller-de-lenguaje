eb6fc8d9b5c89800566ec4631a3f04c3909e1605
alvaro <alvarof260@gmail.com>
2024-03-26 16:08:11 -0300

    Segundo commit puntero

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Para archivos que no quiero que esten expuestos en el repositorio remoto, por ejemplo .env con las variables de entorno.
 solamente hace falta agregar el nombre del archivo o carpeta en forma textual en el archivo .gitignore o la extension del archivo que quisieras ocultar.
+## RESPUESTA 3 E
+
+El archivo que falta es el ignorado.txt por el .gitignore
--- tp1_1.cpp
+++ tp1_1.cpp
@@ -5,5 +5,12 @@ int main()
     int x;
     printf("Hola mundo!");
     scanf("%d", &x);
+    int *p;
+    p = &x;
+    printf("Contenido del puntero: %d\n",*p);
+    printf("Direccion de memoria %p\n", p);
+    printf("Direccion de memoria de la variable %p\n", &x);
+    printf("Direccion de memoria del puntero %p\n", &p);
+    printf("Tamaño de memoria: %d", sizeof(x));
     return 0;
 }
